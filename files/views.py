c88d80efe52fefd75ca65f593b9bd30b2ef441f8
DimaTwist <shmandzmitry@gmail.com>
2021-03-22 22:08:27 +0300

    Commit

--- files/views.py
+++ files/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from services.file_saver_service import FileSavingService
+from services.file_saver_service import FileSavingService, BlobStorage
 # from django.core.files.storage import FileSystemStorage
+from django.core.files import uploadedfile
+import os
 
 
 def upload(request):
     if request.method == "POST":
-        uploaded_file = request.FILES['document']
-        saving = FileSavingService()
-        url = saving.save(uploaded_file.name, uploaded_file)
+        uploaded_file = uploadedfile.UploadedFile(request.FILES['document']) # InMemory object
+        storage = BlobStorage()
+        saving = FileSavingService(uploaded_file, storage)
+        url = saving.save()
 
         return render(request, 'file_upload_form.html', {'url': url})
     else:
--- services/file_saver_service.py
+++ services/file_saver_service.py
@@ -2,10 +2,29 @@
 
 class FileSavingService:
 
-    def save(self, filename_to_load, file_to_load):
-        url = "url"
-        return url
+    def __init__(self, file_to_load, storage):
+        self.storage = storage
+        self.file = file_to_load
 
-# сюда подключаем блоб сторадж
+    def save(self):
+        self.storage.save(self.file)
+        return 'done'
 
 
+class BlobStorage:
+
+    def __init__(self):
+        self.CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=speechrecognitionstorage;AccountKey=7fVulw2eHV1K77IDN4ddKB31+d9RrIGCTbw15akLn8LLJ8IVJAUnB/FU3E3yFrzcv2doaLxdvOooEOodHEPQBQ==;EndpointSuffix=core.windows.net"
+        self.CONTAINER_NAME = "files"
+        self.BLOB_NAME = "blobname"
+
+    def save(self, file_to_load):
+        from azure.storage.blob import BlobClient
+        blob = BlobClient.from_connection_string(self.CONNECTION_STRING,
+                                                 container_name=self.CONTAINER_NAME,
+                                                 blob_name=self.BLOB_NAME)
+
+        with open(file_to_load, 'rb') as data:
+            blob.upload_blob(data, blob_type="BlockBlob")
+
+        return "Loaded"
