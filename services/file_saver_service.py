cd8febcc596bae4de12053aab0808e4a28efedaa
DimaTwist <shmandzmitry@gmail.com>
2021-03-25 20:10:37 +0300

    file_name and file_content_type has been added

--- services/file_saver_service.py
+++ services/file_saver_service.py
@@ -4,24 +4,22 @@ class FileSavingService:
         self.storage = BlobStorage()
 
     def save(self, file_to_load):
-        blob = self.storage.save(file_to_load)
-        print(blob)
-        return blob
+        self.storage.save(file_to_load)
 
 
 class BlobStorage:
 
     CONN_STRING = "DefaultEndpointsProtocol=https;AccountName=speechrecognitionstorage;AccountKey=7fVulw2eHV1K77IDN4ddKB31+d9RrIGCTbw15akLn8LLJ8IVJAUnB/FU3E3yFrzcv2doaLxdvOooEOodHEPQBQ==;EndpointSuffix=core.windows.net"
     CONT_NAME = 'files'
-    BLOB_NAME = 'testfile.wav'
 
     def __init__(self):
-        from azure.storage.blob import BlobClient
-        self.blob = BlobClient.from_connection_string(conn_str=self.CONN_STRING,
-                                                      container_name=self.CONT_NAME,
-                                                      blob_name=self.BLOB_NAME)
+        from azure.storage.blob import BlobServiceClient  # , BlobClient
+        # self.blob = BlobClient.from_connection_string(conn_str=self.CONN_STRING,
+        #                                               container_name=self.CONT_NAME,
+        #                                               blob_name=self.blob_name)
+        self.blob_service = BlobServiceClient.from_connection_string(conn_str=self.CONN_STRING)
 
     def save(self, file):
-        # with open(file, 'rb') as data:
-        self.blob.upload_blob(file, overwrite=True, content_type='audio/wav')
+        self.blob = self.blob_service.get_blob_client(container=self.CONT_NAME, blob=file.name)
+        self.blob.upload_blob(file, overwrite=True, content_type=file.content_type)
 
